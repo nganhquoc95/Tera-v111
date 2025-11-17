7dfd04023887b4d914f30ba27201ff96b8e36854
Nguyen Anh Quoc <nganhquoc95@gmail.com>
2025-11-17 16:48:55 +0700

    Refactor GiveSkillCommand to improve error handling for player and skill validation

--- src/client/messages/commands/gm/GiveSkillCommand.java
+++ src/client/messages/commands/gm/GiveSkillCommand.java
@@ -8,13 +8,23 @@ import client.MapleCharacter;
 public class GiveSkillCommand extends Command { 
     @Override
     public void execute(MapleClient c, String[] splitted) {
-        if (splitted.length < 3) {
+        if (splitted.length < 2) {
             c.getPlayer().dropMessage(5, "Syntax: !giveskill <player> <skillid> [level] [masterlevel]");
             return;
         }
 
         MapleCharacter victim = c.getChannelServer().getPlayerStorage().getCharacterByName(splitted[0]);
+        if (victim == null) {
+            c.getPlayer().dropMessage(5, "Player not found.");
+            return;
+        }
+
         Skill skill = SkillFactory.getSkill(Integer.parseInt(splitted[1]));
+        if (skill == null) {
+            c.getPlayer().dropMessage(5, "Skill not found.");
+            return;
+        }
+
         byte level = (byte) getOptionalIntArg(splitted, 2, 1);
         byte masterlevel = (byte) getOptionalIntArg(splitted, 3, 1);
 
@@ -25,7 +35,9 @@ public class GiveSkillCommand extends Command {
         if (masterlevel > skill.getMaxLevel()) {
             masterlevel = (byte) skill.getMaxLevel();
         }
-        victim.changeSingleSkillLevel(skill, level, masterlevel);   
+
+        victim.changeSingleSkillLevel(skill, level, masterlevel);
+        c.getPlayer().dropMessage(5, "Skill " + skill.getId() + " given to " + victim.getName() + " at level " + level + ".");
     }
 
     public static int getOptionalIntArg(String splitted[], int position, int def) {
