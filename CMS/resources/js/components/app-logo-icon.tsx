d22b3742d82e7f8aaf0cdd529ea31d6f1eeabdb0
Nguyen Anh Quoc <nganhquoc95@gmail.com>
2025-11-20 19:34:35 +0700

    Update AppLogoIcon SVG design and change AppLogo text to 'Nấm Lùn'; fix AppLayout import path

--- CMS/resources/js/components/app-logo-icon.tsx
+++ CMS/resources/js/components/app-logo-icon.tsx
@@ -2,11 +2,41 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <svg {...props} viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
+        <svg {...props} viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+            {/* Maple Leaf Shape */}
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325ZM38 18.2999L32.4 21.411V15.2545L38 12.1434V18.2999ZM36.9409 10.4439L31.4 13.5221L25.8591 10.4439L31.4 7.36561L36.9409 10.4439ZM24.8 18.2999V12.1434L30.4 15.2545V21.411L24.8 18.2999ZM23.8 20.0323L29.3409 23.1105L16.2 30.411L10.6591 27.3328L23.8 20.0323ZM7.6 27.9212L15.2 32.1434V38.2999L2 30.9666V7.92116L7.6 11.0323V27.9212ZM8.6 9.29991L3.05913 6.22165L8.6 3.14339L14.1409 6.22165L8.6 9.29991ZM30.4 24.8101L17.2 32.1434V38.2999L30.4 30.9666V24.8101ZM9.6 11.0323L15.2 7.92117V22.5221L9.6 25.6333V11.0323Z"
+                d="M20 2C20 2 22 8 28 10C34 12 35 18 32 22C30 25 25 26 20 26C15 26 10 25 8 22C5 18 6 12 12 10C18 8 20 2 20 2Z"
+                fill="currentColor"
+            />
+            {/* Left leaf point */}
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M8 18C6 20 2 22 1 28C0 32 4 36 10 36C12 36 14 35 15 33C13 32 9 28 8 24C7 21 8 18 8 18Z"
+                fill="currentColor"
+            />
+            {/* Right leaf point */}
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M32 18C34 20 38 22 39 28C40 32 36 36 30 36C28 36 26 35 25 33C27 32 31 28 32 24C33 21 32 18 32 18Z"
+                fill="currentColor"
+            />
+            {/* Bottom left leaf point */}
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12 28C10 30 6 34 8 38C10 40 16 40 18 38C17 36 15 32 14 28C13 27 12 28 12 28Z"
+                fill="currentColor"
+            />
+            {/* Bottom right leaf point */}
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M28 28C30 30 34 34 32 38C30 40 24 40 22 38C23 36 25 32 26 28C27 27 28 28 28 28Z"
+                fill="currentColor"
             />
         </svg>
     );
