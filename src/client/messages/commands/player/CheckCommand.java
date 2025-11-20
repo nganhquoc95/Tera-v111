bbfabad047fe06982fb6a4d12034cd5d3d4c870d
Nguyen Anh Quoc <nganhquoc95@gmail.com>
2025-11-20 22:14:56 +0700

    Refactor CheckCommand; streamline interaction resolution by removing trade and shop handling, and enhance NPC disposal logic.

--- src/client/messages/commands/player/CheckCommand.java
+++ src/client/messages/commands/player/CheckCommand.java
@@ -3,8 +3,10 @@ package client.messages.commands.player;
 import client.MapleCharacter;
 import client.MapleClient;
 import client.messages.Command;
-import server.MapleTrade;
+import scripting.NPCScriptManager;
 import tools.FileoutputUtil;
+import tools.packet.CWvsContext;
+
 
 public class CheckCommand extends Command{
     {
@@ -13,24 +15,11 @@ public class CheckCommand extends Command{
 
     @Override
     public void execute(MapleClient c, String[] splitted){ 
-        // Fix stuck interact by closing trades and player shops
-        if (c.getPlayer().getTrade() != null) {
-            MapleTrade.cancelTrade(c.getPlayer().getTrade(), c, c.getPlayer());
-            c.getPlayer().dropMessage(6, "Trade cancelled.");
-        }
-        if (c.getPlayer().getPlayerShop() != null) {
-            c.getPlayer().getPlayerShop().closeShop(true, true);
-            c.getPlayer().dropMessage(6, "Player shop closed.");
-        }
-        if (c.getPlayer().getShop() != null) {
-            c.getPlayer().setShop(null);
-            c.getPlayer().dropMessage(6, "Shop closed.");
-        }
-        if (c.getPlayer().getConversation() > 0) {
-            c.getPlayer().setConversation(0);
-            c.getPlayer().dropMessage(6, "NPC conversation closed.");
-        }
-        c.getPlayer().dropMessage(6, "If you were stuck in an interaction, it should be resolved now.");
+        c.removeClickedNPC();
+        c.getPlayer().setConversation(0);
+        NPCScriptManager.getInstance().dispose(c);
+        c.getSession().write(CWvsContext.enableActions());
+
         c.getPlayer().dropMessage(6, "You currently have " + c.getPlayer().getCSPoints(MapleCharacter.CashShopType.NX_CREDIT) + " Cash.");
         c.getPlayer().dropMessage(6, "The time is currently " + FileoutputUtil.CurrentReadable_TimeGMT() + " GMT.");
     }    
