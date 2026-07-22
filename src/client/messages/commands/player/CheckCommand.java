40b90fc8fea9ecbe657e94e6b84ebdb421762df4
quoczz <nganhquoc.zalo@gmail.com>
2026-07-22 20:51:03 +0700

    update check command

--- src/client/messages/commands/player/CheckCommand.java
+++ src/client/messages/commands/player/CheckCommand.java
@@ -6,6 +6,7 @@ import client.messages.Command;
 import scripting.NPCScriptManager;
 import tools.FileoutputUtil;
 import tools.packet.CWvsContext;
+import server.ServerProperties;
 
 
 public class CheckCommand extends Command{
@@ -19,8 +20,12 @@ public class CheckCommand extends Command{
         c.getPlayer().setConversation(0);
         NPCScriptManager.getInstance().dispose(c);
         c.getSession().write(CWvsContext.enableActions());
+        String expRate = ServerProperties.getProperty("net.sf.odinms.world.exp");
+        String mesoRate = ServerProperties.getProperty("net.sf.odinms.world.meso");
+        String dropRate = ServerProperties.getProperty("net.sf.odinms.world.dropRate");
 
+        c.getPlayer().dropMessage(6, "Server: EXP: " + expRate + "x, MESO: " + mesoRate + "x, DROP: " + dropRate + "x");
         c.getPlayer().dropMessage(6, "You currently have " + c.getPlayer().getCSPoints(MapleCharacter.CashShopType.NX_CREDIT) + " Cash.");
         c.getPlayer().dropMessage(6, "The time is currently " + FileoutputUtil.CurrentReadable_TimeGMT() + " GMT.");
-    }    
+    }
 }
