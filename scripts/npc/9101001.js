9560986b672de09bfe9f168ca0744c6a6968178a
Nguyen Anh Quoc <nganhquoc95@gmail.com>
2026-08-07 10:46:39 +0700

    auto format files

--- scripts/npc/9101001.js
+++ scripts/npc/9101001.js
@@ -6,18 +6,18 @@
 var status = -1;
 
 function action(mode, type, selection) {
-    if (mode == 1) {
-	status++;
-    } else {
-	status--;
-    }
-    if (status == 0) {
-	cm.sendNext("You have finished all your trainings. Good job. You seem to be ready to start with the journey right away! Good, I will let you move on to the next place.");
-    } else if (status == 1) {
-	cm.sendNextPrev("But remember, once you get out of here, you will enter a village full with monsters. Well them, good bye!");
-    } else if (status == 2) {
-	cm.warp(40000, 0);
-	cm.gainExp(3);
-	cm.dispose();
-    }
+	if (mode == 1) {
+		status++;
+	} else {
+		status--;
+	}
+	if (status == 0) {
+		cm.sendNext("You have finished all your trainings. Good job. You seem to be ready to start with the journey right away! Good, I will let you move on to the next place.");
+	} else if (status == 1) {
+		cm.sendNextPrev("But remember, once you get out of here, you will enter a village full with monsters. Well them, good bye!");
+	} else if (status == 2) {
+		cm.warp(40000, 0);
+		cm.gainExp(3);
+		cm.dispose();
+	}
 }
